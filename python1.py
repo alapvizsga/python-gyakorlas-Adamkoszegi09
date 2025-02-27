f6745e02d07cc385ad416a1c6fed5cf9e90c57c4
Adamkoszegi09 <135693328+Adamkoszegi09@users.noreply.github.com>
2025-02-27 15:15:18 +0100

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,228 @@
+#--------------------------
+'''
+A kettovel_oszthato nevű függvény, 
+egy számot kap bemenetként és 
+visszatér True-val, ha a szám kettővel osztható és 
+visszatér False-al ha a szám kettővel nem ossztható.
+'''
+def kettovel_oszthato(szam):
+    return szam % 2 == 0
+
+
+
+
+#--------------------------
+'''
+A teglatest_terfogat nevű függvény, 
+bemenetként megkapja a téglatest oldalainak hosszúságát és 
+a téglatest térfogatával tér vissza.
+'''
+def teglatest_terfogat(a,b,c):
+    return a * b * c
+
+
+#--------------------------
+'''
+A derekszogu_haromszog_terulet nevű függvény, 
+bemenetként megkapja a befogók hosszát és 
+a háromszög területével tér vissza.
+'''
+def derekszogu_haromszog_terulet(a,b):
+    return a * b /2
+
+
+#--------------------------
+'''
+Az abszolut nevű függvény, 
+a bemenő paraméterként kapott szám 
+abszolút értékével tér vissza.
+'''
+def abszolut(szam):
+    if(szam<0):
+        return -szam
+    return szam
+        
+
+#--------------------------
+'''
+A kor_terulete nevű függvény, 
+bemenő paraméterként kapja a kör sugarát és 
+visszatér a kör területével.
+'''
+
+
+
+#--------------------------
+'''
+A maradek nevű függvény,
+két számot kap bemenetként és 
+visszatér a két szám hányadosának maradékával.
+'''
+
+
+
+#--------------------------
+'''
+A teglalap_kerulet nevű függvény, 
+egy téglalap oldalhosszait kapja bemenetként és 
+visszatér a téglalap kerületével.
+'''
+
+
+
+#--------------------------
+'''
+A kisebb nevű függvény,
+két számot kap és 
+visszatér a kisebbik számmal.
+
+Nem használhatod a min nevű függvényt
+
+'''
+
+
+
+#--------------------------
+'''
+A harommal_oszthato nevű függvény,
+egy számot kap bemenetként és 
+True-val tér vissza, ha a szám hárommal osztható és 
+False-al ha nem.
+'''
+
+
+
+#--------------------------
+'''
+A kor_kerulete nevű függvény 
+bemenő paraméterként kapja a kör sugarát és 
+visszatér a kör kerületével.
+'''
+
+
+
+#--------------------------
+'''
+A negyzet_kerulet nevű függvény, 
+egy négyzet oldalhosszát kapja bemenetként és 
+visszatér a négyzet kerületével.
+'''
+
+
+
+#--------------------------
+'''
+A kulonbseg nevű függvény,
+két számot kap bemenetként és 
+visszatér a két szám különbségével.
+'''
+
+
+
+#--------------------------
+'''
+A negyzet_terulet nevű függvény,
+egy négyzet oldalhosszát kapja bemenetként és 
+visszatér a négyzet területével.
+'''
+
+
+
+#--------------------------
+'''
+Az osszead nevű nevű függvény,
+két számot kap és 
+visszatér a két szám összegével.
+
+Nem használhatod a sum nevű függvényt!
+    
+'''
+
+
+
+#--------------------------
+'''
+A kocka_terfogat nevű függvény,
+bemenetként megkapja a kocka oldal hosszúságát és 
+a kocka térfogatával tér vissza.
+'''
+
+
+
+#--------------------------
+'''
+A derekszogu_haromszog_atfogo nevű függvény, 
+bemenetként megkapja a befogók hosszát és 
+az átfogó hosszával tér vissza.
+'''
+
+
+
+#--------------------------
+'''
+A teglalap_terulet nevű függvény,
+egy téglalap oldalhosszait kapja bemenetként és 
+visszatér a téglalap területével.
+'''
+
+
+
+#--------------------------
+'''
+A hettel_oszthato nevű függvény, 
+egy számot kap bemenetként és 
+True-val tér vissza, ha a szám héttel osztható és 
+False-al ha nem.
+'''
+
+
+
+#--------------------------
+'''
+A negyzet_atloja nevű függvény, 
+bemenetként megkapja a négyzet oldalának hosszát és 
+az átló hosszával tér vissza.
+'''
+
+
+
+#--------------------------
+'''
+A teglalap_atloja nevű függvény, 
+bemenetként megkapja az oldalak hosszát és 
+az átló hosszával tér vissza.
+'''
+
+
+
+#--------------------------
+'''
+A nagyobb nevű függvény,
+két számot kap és 
+visszatér a nagyobb számmal.
+
+Nem használhatod a max nevű függvényt.
+'''
+
+
+
+#--------------------------
+'''
+A paros nevű függvény,
+egy számot kap bemenetként, 
+majd True-val tér vissza, ha a szám páros és 
+False-al ha a szám páratlan.
+'''
+
+
+
+#--------------------------
+'''
+A szamtani_kozep nevű függvény, 
+két számot kap és 
+visszatér a számtani középpel.
+'''
+
+
+
+#======================================================================================================================C:\Users\munka\Downloads\python-gyakorlas-Adamkoszegi09-main\python-gyakorlas-Adamkoszegi09-main
